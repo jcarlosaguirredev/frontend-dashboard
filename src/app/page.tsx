f4fd03f2a574b2d8ea2368a3411f11df0cb40719
jcarlosaguirredev <jcarlosaguirre.dev@gmail.com>
2025-12-23 11:35:17 +0100

    feat: explore render cycle and custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+"use client";
+
+import { useCounter } from "@/app/hooks/useCounter";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home() {
+  console.log("Rendering Home Component");
+
+  const [count, _setCount] = useState(0);
+  const renders = useRef(0);
+  const counter = useCounter();
+
+  useEffect(() => {
+    renders.current++;
+    console.log(`REnders ${renders.current} times`);
+  }, []);
+
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Frontend Dashboard</h1>
+    <main className="p-6 space-y-4">
+      <p>Count: {counter.count}</p>
+      <p>Count: {count}</p>
+      <button
+        className="px-4 py-2 bg-black text-white"
+        onClick={counter.increment}
+      >
+        Increment
+      </button>
+      <button
+        className="px-4 py-2 bg-black text-white"
+        onClick={counter.decrement}
+      >
+        Decrement
+      </button>
     </main>
   );
 }
